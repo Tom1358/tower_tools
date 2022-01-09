f2f9d25bfc8bb3aec53a622a93f96a14c16c0e1d
Tom1358 <thomasellis358@hotmail.com>
2022-01-09 13:30:24 +0000

    Create search functionality and confirm working - search.html template created

--- home/views.py
+++ home/views.py
@@ -1,7 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
+from products.models import Product
 
 def index(request):
     """ a view to return the index page """
 
-
     return render(request, 'home/index.html')
+
+
+def search(request):
+    """ a view to search products """
+
+    products = Product.objects.all()
+    query = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "wrong!")
+                return redirect(reverse(''))
+            
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            products = products.filter(queries)
+
+    context = {
+        'products': products,
+        'search_term': query,
+    }
+
+    return render(request, 'search.html', context)
