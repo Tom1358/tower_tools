4b552130bdbc83be8a773facdbcc21d4f89390ce
Tom1358 <thomasellis358@hotmail.com>
2022-01-09 18:25:05 +0000

    Add qty to shopping basket icon on homepage navbar

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,17 +1,34 @@
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from products.models import Product
+
 
 def bag_contents(request):
 
     bag_items = []
+    free_delivery_threshold = 50
+    standard_delivery_percentage = 0.1
     total = 0
     product_count = 0
+    bag = request.session.get('bag', {})
 
-    if total < 50:
+    for item_id, quantity in bag.items():
+        product = get_object_or_404(Product, pk=item_id)
+        total += quantity * product.price
+        product_count += quantity
+        bag_items.append({
+            'item-id': item_id,
+            'quantity': quantity,
+            'product': product,
+        })
+
+
+    if total < free_delivery_threshold:
         # Free delivery over £50
-        delivery = total * Decimal(0.1)
+        delivery = total * Decimal(standard_delivery_percentage)
         # If less than £50, delivery charged at 10% of all items
-        remaining_to_free_del = 50 - total
+        remaining_to_free_del = free_delivery_threshold - total
     else:
         delivery = 0
         remaining_to_free_del = 0
@@ -20,9 +37,12 @@ def bag_contents(request):
 
     context = {
         'bag_items': bag_items,
+        'quantity': quantity,
         'total': total,
         'product_count': product_count,
         'delivery': delivery,
         'remaining_to_free_del': remaining_to_free_del,
         'grand_total': grand_total,
     }
+
+    return context
