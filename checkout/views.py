b12a839e46553dd7ef53d0ab8c7d29c90d7e3548
Tom1358 <thomasellis358@hotmail.com>
2022-01-22 12:09:18 +0000

    Create view for stripe payment in checkout app

--- checkout/views.py
+++ checkout/views.py
@@ -1,19 +1,37 @@
 import os
 from django.shortcuts import render, redirect, reverse
+from django.conf import settings
+from bag.contexts import bag_contents
 
 from .forms import OrderForm
+import stripe
 
 
 def checkout(request):
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
     bag = request.session.get('bag', {})
     if not bag:
         return redirect(reverse('home'))
     
+    current_bag = bag_contents(request)
+    total = current_bag['grand_total']
+    stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
+
+    order_form = OrderForm()
+
     order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': os.environ.get('STRIPE_PUBLIC_KEY'),
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)
