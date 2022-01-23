99d434458558fce0c170a55a4fd35a4dcac144b2
Tom1358 <thomasellis358@hotmail.com>
2022-01-23 11:37:21 +0000

    Allow user's info to be saved to profile upon ordering - updated Checkout app views.py

--- checkout/views.py
+++ checkout/views.py
@@ -5,6 +5,9 @@ from django.conf import settings
 
 from bag.contexts import bag_contents
 from products.models import Product
+from profiles.forms import UserProfileForm
+from profiles.models import UserProfile
+
 from .models import Order, OrderLineItem
 from .forms import OrderForm
 import stripe
@@ -27,9 +30,10 @@ def checkout(request):
             'street_address1': request.POST['street_address1'],
             'street_address2': request.POST['street_address2'],
         }
-        order_form = OrderForm(form_data)
 
+        order_form = OrderForm(form_data)
         order = order_form.save()
+        
         for item_id, item_data in bag.items():
             product = Product.objects.get(id=item_id)
             if isinstance(item_data, int):
@@ -56,7 +60,23 @@ def checkout(request):
             currency=settings.STRIPE_CURRENCY,
         )
 
-        order_form = OrderForm()
+        if request.user.is_authenticated:
+            try:
+                profile = UserProfile.objects.get(user=request.user)
+                order_form = OrderForm(initial={
+                    'full_name': profile.user.get_full_name(),
+                    'email': profile.user.email,
+                    'phone_number': profile.default_phone_number,
+                    'country': profile.default_country,
+                    'postcode': profile.default_postcode,
+                    'town_or_city': profile.default_town_or_city,
+                    'street_address1': profile.default_street_address1,
+                    'street_address2': profile.default_street_address2,
+                })
+            except UserProfile.DoesNotExist:
+                order_form = OrderForm()
+        else:
+            order_form = OrderForm()
 
     template = 'checkout/checkout.html'
     context = {
@@ -74,6 +94,26 @@ def checkout_success(request, order_number):
     """
     save_info = request.session.get('save_info')
     order = get_object_or_404(Order, order_number=order_number)
+
+    if request.user.is_authenticated:
+        profile = UserProfile.objects.get(user=request.user)
+
+        order.user_profile = profile
+        order.save()
+
+        if save_info:
+            profile_data = {
+                'default_phone_number': order.phone_number,
+                'default_street_address1': order.street_address1,
+                'default_street_address2': order.street_address2,
+                'default_town_or_city': order.town_or_city,
+                'default_country': order.country,
+                'default_postcode': order.postcode,
+            }
+            user_profile_form = UserProfileForm(profile_data, instance=profile)
+            if user_profile_form.is_valid():
+                user_profile_form.save()
+
     messages.success(request, f'Order successfully processed! \
         Your order number is {order_number}.')
 
