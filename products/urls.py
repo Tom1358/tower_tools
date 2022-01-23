39432b80f9693be6f82b19048fa83840884b92dd
Tom1358 <thomasellis358@hotmail.com>
2022-01-23 14:39:47 +0000

    Add view for adding a product to the site

--- products/urls.py
+++ products/urls.py
@@ -4,8 +4,9 @@ from . import views
 
 
 urlpatterns = [
-    path('ringing_equipment', views.ringing_equipment, name='products/ringing_equipment'),
-    path('merchandise', views.merchandise, name='products/merchandise'),
-    path('learning_tools', views.learning_tools, name='products/learning_tools'),
-    path('<product_id>', views.product_detail, name='product_detail'),
+    path('ringing_equipment/', views.ringing_equipment, name='products/ringing_equipment'),
+    path('merchandise/', views.merchandise, name='products/merchandise'),
+    path('learning_tools/', views.learning_tools, name='products/learning_tools'),
+    path('<int:product_id>/', views.product_detail, name='product_detail'),
+    path('add/', views.add_product, name='add_product'),
 ]
--- products/views.py
+++ products/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
 from .models import Product
+from .forms import ProductForm
 
 def ringing_equipment(request):
     """ a view to return ringing equipment products """
@@ -49,3 +50,15 @@ def product_detail(request, product_id):
     }
 
     return render(request, 'products/product_detail.html', context)
+
+
+def add_product(request):
+    """ Add a product to the site """
+    form = ProductForm()
+    template = 'products/add_product.html'
+
+    context = {
+        'form': form;
+    }
+
+    return render(request, template, context)
